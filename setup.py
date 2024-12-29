e79476c98087c292a8b51bef6b84bc0314cc500d
Mr RajabZade <146377979+MrRajabZade@users.noreply.github.com>
2024-12-29 12:45:31 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
 import os
 
-os.system("pip install requests langdetect selenium pynput pywin32 pillow threading colorama")
+# لیست تمام کتابخانه‌هایی که نیاز دارید
+libraries = [
+    "requests",
+    "comtypes",
+    "pillow",
+    "selenium",
+    "colorama",
+    "soundcard",
+    "soundfile",
+    "pycaw",
+    "pywin32"
+]
+
+# نصب هر کتابخانه
+for library in libraries:
+    print(f"Installing {library}...")
+    os.system(f"pip install {library}")
+    print(f"{library} installation finished.\n")
